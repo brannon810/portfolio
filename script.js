00992a6c9a25c9fe0165d92b6eab0f7816eda9c1
brannon810 <stewartb@students.westerntc.edu>
2024-10-12 01:10:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navbtns = document.querySelector("nav").querySelector("div").querySelectorAll("a")
+    const sections = document.querySelector("div#pages").querySelectorAll("section")
+    let shownav = true
+    document.getElementById("home-btn").style.color = "grey"
+
+    document.getElementById("hide-show").addEventListener("click", function () {
+        if (shownav == true) {
+            shownav = false
+            document.querySelector("nav").querySelector("div").style.display = "none"
+            document.querySelector("nav").style.width = "20px"
+            document.getElementById("pages").style.width = "99%"
+            document.getElementById("pages").style.left = "20px"
+            document.getElementById("hide-show").innerText = ">"
+        } else {
+            shownav = true
+            document.querySelector("nav").querySelector("div").style.display = "block"
+            document.querySelector("nav").style.width = "10%"
+            document.getElementById("pages").style.width = "89%"
+            document.getElementById("pages").style.left = "160px"
+            document.getElementById("hide-show").innerText = "<"
+        }
+    })
+
+    function load(id) {
+        const pageid = id.slice(0, -4)
+        sections.forEach(page => {
+            page.style.display = "none"
+        })
+        navbtns.forEach(btn => {
+            btn.removeAttribute("style")
+        })
+        document.getElementById(id).style.color = "grey"
+        document.getElementById(pageid).style.display = "inline-block"
+    }
+
+    navbtns.forEach(btn => {
+        btn.addEventListener('click', function () {
+            const id = btn.id
+            load(id)
+        })
+    })
+})
